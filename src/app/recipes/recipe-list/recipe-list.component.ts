70028f93b8f24c3c2f17ab76b0d06df7d11df273
bharath-developer <96855386+bharath-developer@users.noreply.github.com>
2022-02-16 16:23:34 +0530

    second commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -16,9 +16,9 @@ export class RecipeListComponent implements OnInit {
       'https://media.istockphoto.com/photos/chicken-curry-picture-id1124973765?k=20&m=1124973765&s=612x612&w=0&h=y8JcYe_oxCdKdP_4RpFG5opaygOasZUKdEqT35nVu6c='
     ),
     new Recipe(
-      'curry',
-      'spicy indian curry',
-      'https://media.istockphoto.com/photos/chicken-curry-picture-id1124973765?k=20&m=1124973765&s=612x612&w=0&h=y8JcYe_oxCdKdP_4RpFG5opaygOasZUKdEqT35nVu6c='
+      'fun fuzed fruit',
+      'a healthy bowl of fruits and its good for health',
+      'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277__480.jpg'
     ),
   ];
 
--- src/app/shopping-list/shoppinglist.component.ts
+++ src/app/shopping-list/shoppinglist.component.ts
@@ -13,4 +13,7 @@ export class ShoppingList implements OnInit {
   ];
   constructor() {}
   ngOnInit(): void {}
+  addIngredient(ingrdeintData: Ingredient) {
+    this.ingrdients.push(ingrdeintData);
+  }
 }
